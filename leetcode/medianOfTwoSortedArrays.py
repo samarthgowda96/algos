e361ec2f643aaf02104c8f0ee457289373e7ed81
Samarth Gowda <samarthgowda@Samarths-MBP.fios-router.home>
2021-06-01 16:48:42 -0400

    findMedianSortedArrays

---
+++ leetcode/medianOfTwoSortedArrays.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMedianSortedArrays(self,nums1,nums2):
+        for i in range(len(nums2)):
+            nums1.append(nums2[i])
+        nums1.sort()
+        if(len(nums1)%2==0):
+            mid= len(nums1)//2
+            median = (nums1[mid-1]+nums1[mid])//2
+        else:
+            mid= len(nums1)//2
+            median= nums1[mid]
+        return median
+
+sol=Solution()
+nums1=[1,3]
+nums2=[2]
+print(sol.findMedianSortedArrays(nums1,nums2))
+
+
