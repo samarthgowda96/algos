05b48a5e58555c0fa88bd9a97e7b786cf74c44d9
Samarth Gowda <samarthgowda@samarths-mbp.lan>
2021-06-11 12:57:44 -0700

    selection, insertion, fiboMem

--- fiboMem.py
+++ fiboMem.py
@@ -1,13 +1,14 @@
 def fib(n,memo):
-    if memo[n]!= None:
+    if memo[n] is not None:
         return memo[n]
     if n==1 or n==2:
         result =1
     else:
-        result= fib(n-1) + fib(n-2)
+        result= fib(n-1,memo) + fib(n-2,memo)
         memo[n]=result 
     return result
+def fib_memo(n):
+    memo= [None]*(n+1)
+    return fib(n,memo)
 
-n= 50
-
-print(fib(n,0))
+print(fib_memo(50))
