e97e35e369ee202bb0c3cb2b360b8152cbcc4993
Samarth Gowda <samarthgowda@samarths-mbp.lan>
2021-06-22 23:51:35 -0700

    Working

--- HackerRank/fraudualentAct.py
+++ HackerRank/fraudualentAct.py
@@ -1,40 +1,64 @@
-def frauds(exp, d):
-    count=0
+# def frauds(exp, d):
+#     count=0
   
-    for i in range(0,len(exp)):
+#     for i in range(0,len(exp)):
             
-            if(d%2)==0:
-                j=i
-                temp=exp[j:d+1]
-                print(temp)
-                mid= len(temp)//2
-                #print(temp[mid],temp[mid-1])
-                median=(temp[mid]+temp[mid-1])/2
-                res= (2*median)
+#             if(d%2)==0:
+#                 j=i
+#                 temp=exp[j:d+1]
+#                 temp.sort()
                
-                if (len(exp)>d+1):
-                    if exp[d+1]>=res:
-                        count+=1
+#                 mid= len(temp)//2
+#                 median=(temp[mid]+temp[mid-1])/2
+#                 res= (2*median)
                
-                
-                
-            else:
-                temp=exp[i:d:1]
+#                 if (len(exp)>d+1):
+#                     if exp[d+1]>=res:
+#                         count+=1
+               
+#             else:
+#                 temp=exp[i:d:1]
             
-                mid= len(temp)//2
-                median= temp[mid]
-                res=2*median
+#                 mid= len(temp)//2
+#                 median= temp[mid]
+#                 res=2*median
+#                 if (len(exp)>d+1):
+#                     if exp[d+1]>=res:
+#                         count+=1
+#             d=d+1
+#     return count
+
+import numpy as np  
+def checkfrauds(exp, d):
+    print ("-"*20)
+    print ("Checking Fraud")
+    print ("-"*20)
+    
+    # Initialize Alert Count Variable to 0
+    alert_count = 0
+
+    # Set n as the length of exp
+    n = len(exp)
+
+    for i in range(d, n):
+
+        prev_d_exps = exp[i-d: i]
+
+        if exp[i] >= np.median(prev_d_exps) * 2:
+            alert_count+=1
+            print('Sending Alert')
+
+        print("{} - Prev Exp - {} - Median : {}".format( exp[i], prev_d_exps, np.median(prev_d_exps) ))
+
+    print("n : {}".format(n))
+    print ("-"*20)
+    print("alert_count : {}".format(alert_count))
+    return alert_count
                
-                """ if (len(exp)>=d+1):
-                    if exp[d+1]>=res:
-                        count+=1 """
-            d=d+1
-    return count
-                
 
-               
+#exp=[1,4,5,6,7,43,3]
+exp=[10,20,30,40,50]
 
-exp=[1,4,5,6,7,43,3]
-d= 2
+d= 3
 
-print(frauds(exp,d))
+checkfrauds(exp, d)
