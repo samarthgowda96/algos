fa47b7258138322c9f2dff47860ba169b28ba0b5
Samarth Gowda <samarthgowda13@gmail.com>
2022-04-25 16:57:21 -0400

    last Stonyy

---
+++ 2022/lastStone.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        while len(stones) >1:
+            firstMaxStone = max(stones)
+            stones.remove(firstMaxStone)
+            if stones:
+                secondMaxStone = max(stones)
+                stones.remove(secondMaxStone)
+           
+            remaining = abs(firstMaxStone - secondMaxStone)
+            stones.append( remaining)
+                
+        if len(stones):
+            return stones[0]
+        else:
+            return 0
